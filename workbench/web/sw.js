c0b1af1b70fea656a17c810c910906046b167f97
qqkiky <qqkiky@users.noreply.github.com>
2026-09-26 02:15:23 +0800

    chore: 工作台定时同步 2026-09-26 02:15

--- workbench/web/sw.js
+++ workbench/web/sw.js
@@ -1,5 +1,5 @@
 
-const BUILD = "20260924021247";
+const BUILD = "20260926021523";
 const SHELL = ['index.html','manifest.json','icon-192.png','icon-512.png','version.json'];
 const CACHE = 'wb-shell-' + BUILD;
 self.addEventListener('install', function(e){
